c9ce9060aea74ef93ac5282439cee7114812df0c
Tianyu Wang <terrywang@tianyu.ninja>
2019-09-06 17:12:05 -0400

    test deploy

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-})
+  res.send({hi: 'success!'});
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
