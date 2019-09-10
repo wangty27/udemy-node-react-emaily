5b65c6b1a498bdfec1e2b312793af827d1b5f0b9
Tianyu Wang <terrywang@tianyu.ninja>
2019-09-09 22:26:26 -0400

    fixed proxy

--- services/passport.js
+++ services/passport.js
@@ -18,7 +18,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.GoogleClientID,
     clientSecret: keys.GoogleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
   }, (accessToken, refreshToken, profile, done) => {
     User.findOne({ googleID: profile.id }).then(existingUser => {
       if (existingUser) {
