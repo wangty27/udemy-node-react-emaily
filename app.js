d9d12670f1906d5c66e612006819a85f7d02c8fc
Tianyu Wang <terrywang@tianyu.ninja>
2019-09-09 22:20:50 -0400

    finished auth flow

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hi: 'success!'});
-});
+app.use(cookieSession({ maxAge: 24 * 60 * 60 * 1000, keys: [keys.cookieKey] }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-const PORT = process.env.PORT || 3000;
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+  GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+}
